7a3623647b20c39cf8fc45990d8e3375aa4f2963
j.f <usausa13108891@gmail.com>
2021-12-29 10:34:39 +0000

    posy

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
+  
   resources :users
+  
+  resources :microposts, only: [:create, :destroy]
+
 end
