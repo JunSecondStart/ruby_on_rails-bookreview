e5021cb460702d47c018885eaf283a89567dc08d
j.f <usausa13108891@gmail.com>
2021-12-31 17:56:20 +0000

    review delete OK

--- app/models/review.rb
+++
@@ -1,9 +0,0 @@
-class Review < ApplicationRecord
-  belongs_to :user
-  belongs_to :book
-  
-  validates :content, presence: true, length: { maximum: 255 }
-  validates :star, numericality: { in: 1..5 }
-  validates :heart, numericality: { in: 1..5 }
-  validates :intelligence, numericality: { in: 1..5 }
-end
