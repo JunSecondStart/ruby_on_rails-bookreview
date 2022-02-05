31d1258a41a9dd81347dde26363a7d673066a463
j.f <usausa13108891@gmail.com>
2022-02-05 09:35:19 +0000

    bookreview1

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
  # rakuten_web_service内のclassで使えるようにアプリケーションIDを設定 <注意>アプリケーションIDをつけたままgitにあげてはいけない（使われまくるから）
   RakutenWebService.configure do |c|
-    c.application_id = ''
+    c.application_id = '1020138923467456239'
   end
